5287d94a03828550dab126a129747d9df219dc65
tuxxy <me@johnpacific.com>
2017-12-05 15:20:33 -0700

    Add placeholders for EC_POINT arithmetic

--- hazmat_math/operations.py
+++ hazmat_math/operations.py
@@ -39,3 +39,36 @@ def BN_MOD_SUB(priv_a, priv_b) -> ec.EllipticCurvePrivateKey:
     result in an EllipticCurvePrivateKey.
     """
     pass
+
+
+def EC_POINT_MUL(pub_factor1, priv_factor2) -> ec.EllipticCurvePublicKey:
+    """
+    Performs an OpenSSL EC_POINT_mul on an EllipticCurvePublicKey with an
+    EllipticCurvePrivateKey and returns the result in an EllipticCurvePublicKey.
+    """
+    pass
+
+
+def EC_POINT_INVERT(pub_a) -> ec.EllipticCurvePublicKey:
+    """
+    Performs an OpenSSL EC_POINT_invert on an EllipticCurvePublicKey and returns
+    the result in an EllipticCurvePublicKey.
+    """
+    pass
+
+
+def EC_POINT_ADD(pub_a, pub_b) -> ec.EllipticCurvePublicKey:
+    """
+    Performs an OpenSSL EC_POINT_add on two EllipticCurvePublicKeys and returns
+    the result in an EllipticCurvePublicKey.
+    """
+    pass
+
+
+def EC_POINT_SUB(pub_a, pub_b) -> ec.EllipticCurvePublicKey:
+    """
+    Performs subtraction by adding an EllipticCurvePublicKey to the inverse of
+    another EllipticCurvePublicKey and returns the result in an
+    EllipticCurvePublicKey.
+    """
+    pass
